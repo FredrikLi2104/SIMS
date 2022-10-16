849d8623e5e227fd0cd6dd6e4b58c707e060d48d
anon <veritasconceptsuae@gmail.com>
2022-10-16 18:06:35 +0400

    2022-10-16|S9

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 set -e
 cd /var/www/html/backend.gdpr.se
 echo "Deployment started ..."
+ls -la ~/.ssh
 git pull origin master
 #only on start or adding a composer package
 #composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
