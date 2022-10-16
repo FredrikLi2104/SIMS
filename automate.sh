285cf7c47a5a73afa1fa06513f7a17eb352fd624
anon <veritasconceptsuae@gmail.com>
2022-10-16 20:14:01 +0400

    2022-10-16|S89

--- automate.sh
+++
@@ -1,3 +0,0 @@
-git pull origin master
-php artisan migrate --force
-php artisan db:seed --force
--- expect.sh
+++
@@ -1,3 +0,0 @@
-spawn ./automate.sh
-expect -exact "Enter passphrase for key '/home/gdpr_root/.ssh/id_ed25519': "
-send -- "@VLXSaea_rr"
