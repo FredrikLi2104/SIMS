941144698b7584b35b057f7ac5275cab40cd95dc
anon <veritasconceptsuae@gmail.com>
2022-10-16 18:07:46 +0400

    2022-10-16|S10

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@ set -e
 cd /var/www/html/backend.gdpr.se
 echo "Deployment started ..."
 ls -la ~/.ssh
-git pull origin master
+#git pull origin master
 #only on start or adding a composer package
 #composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
-php artisan migrate --force
-php artisan db:seed --force
+#php artisan migrate --force
+#php artisan db:seed --force
 echo "Deployment finished!"
