f5614be28e4bbd8129c2dbfce7224eb50ee729ed
anon <veritasconceptsuae@gmail.com>
2022-10-17 09:22:18 +0400

    2022-10-17|S9899

--- expect.sh
+++ expect.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/expect -f
 spawn ./spawn.sh
 expect -exact "Enter passphrase for key '/home/gdpr_root/.ssh/id_ed25519': "
-send -- "@VLXSaea_rr"
+send -- "@VLXSaea_rr\r"
--- spawn.sh
+++ spawn.sh
@@ -1,3 +1,6 @@
 git pull origin master
+echo "pulled"
 php artisan migrate --force
+echo "migrated"
 php artisan db:seed --force
+echo "seeded"
