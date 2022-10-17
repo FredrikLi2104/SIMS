04ff0c91bc2e12a78d9f614c1835e4a65a7d546e
anon <veritasconceptsuae@gmail.com>
2022-10-17 18:21:51 +0400

    2022-10-17|statements.na

--- resources/views/models/statements/create.blade.php
+++ resources/views/models/statements/create.blade.php
@@ -32,7 +32,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="content_en">@lang('messages.content_en')</label>
-                                        <textarea id="content_en" class="form-control @error('content_en') is-invalid @enderror" rows="3" placeholder="We have defined a lawful basis for all processes." name="content_en">{{ old('content_en') }}</textarea>
+                                        <textarea id="content_en" class="form-control @error('content_en') is-invalid @enderror" rows="3" placeholder="We have defined a lawful basis for all processes." name="content_en">N/A</textarea>
                                         @error('content_en')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -41,7 +41,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="content_se">@lang('messages.content_se')</label>
-                                        <textarea id="content_se" class="form-control @error('content_se') is-invalid @enderror" rows="3" placeholder="Vi har definierat en laglig grund för alla processer." name="content_se">{{ old('content_se') }}</textarea>
+                                        <textarea id="content_se" class="form-control @error('content_se') is-invalid @enderror" rows="3" placeholder="Vi har definierat en laglig grund för alla processer." name="content_se">N/A</textarea>
                                         @error('content_se')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -50,7 +50,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="desc_en">@lang('messages.desc_en')</label>
-                                        <textarea id="desc_en" class="form-control @error('desc_en') is-invalid @enderror" rows="3" placeholder="It means that all personal data process need a specific legal support according to DGPR. Without a legal support the process is unlawful. De lawful basis are regulated in art. 6 GDPR and for sensitive personal data, one extra lawful basis according to art. 9 GDPR is needed. " name="desc_en">{{ old('desc_en') }}</textarea>
+                                        <textarea id="desc_en" class="form-control @error('desc_en') is-invalid @enderror" rows="3" placeholder="It means that all personal data process need a specific legal support according to DGPR. Without a legal support the process is unlawful. De lawful basis are regulated in art. 6 GDPR and for sensitive personal data, one extra lawful basis according to art. 9 GDPR is needed. " name="desc_en">N/A</textarea>
                                         @error('desc_en')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -59,7 +59,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="desc_se">@lang('messages.desc_se')</label>
-                                        <textarea id="desc_se" class="form-control @error('desc_se') is-invalid @enderror" rows="3" placeholder="Det innebär att alla personuppgifter behöver ett specifikt juridiskt stöd enligt DGPR. Utan juridiskt stöd är processen olaglig. De rättsliga grunderna regleras i artikel 6 GDPR och för känsliga personuppgifter krävs en extra laglig grund enligt artikel 9 GDPR." name="desc_se">{{ old('desc_se') }}</textarea>
+                                        <textarea id="desc_se" class="form-control @error('desc_se') is-invalid @enderror" rows="3" placeholder="Det innebär att alla personuppgifter behöver ett specifikt juridiskt stöd enligt DGPR. Utan juridiskt stöd är processen olaglig. De rättsliga grunderna regleras i artikel 6 GDPR och för känsliga personuppgifter krävs en extra laglig grund enligt artikel 9 GDPR." name="desc_se">N/A</textarea>
                                         @error('desc_se')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -68,7 +68,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="k1_en">k1 @lang('messages.inEnglish') [@lang('messages.optional')]</label>
-                                        <textarea id="k1_en" class="form-control @error('k1_en') is-invalid @enderror" rows="3" placeholder="We have a strategy or action plan, or we have regulated it by agreement" name="k1_en">{{ old('k1_en') }}</textarea>
+                                        <textarea id="k1_en" class="form-control @error('k1_en') is-invalid @enderror" rows="3" placeholder="We have a strategy or action plan, or we have regulated it by agreement" name="k1_en">N/A</textarea>
                                         @error('k1_en')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -77,7 +77,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="k1_se">k1 @lang('messages.inSwedish') [@lang('messages.optional')]</label>
-                                        <textarea id="k1_se" class="form-control @error('k1_se') is-invalid @enderror" rows="3" placeholder="Vi har en strategi eller handlingsplan, alternativt reglerat det genom avtal" name="k1_se">{{ old('k1_se') }}</textarea>
+                                        <textarea id="k1_se" class="form-control @error('k1_se') is-invalid @enderror" rows="3" placeholder="Vi har en strategi eller handlingsplan, alternativt reglerat det genom avtal" name="k1_se">N/A</textarea>
                                         @error('k1_se')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -86,7 +86,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="k2_en">k2 @lang('messages.inEnglish') [@lang('messages.optional')]</label>
-                                        <textarea id="k2_en" class="form-control @error('k2_en') is-invalid @enderror" rows="3" placeholder="We have a strategy or action plan, or we have regulated it by agreement" name="k2_en">{{ old('k2_en') }}</textarea>
+                                        <textarea id="k2_en" class="form-control @error('k2_en') is-invalid @enderror" rows="3" placeholder="We have a strategy or action plan, or we have regulated it by agreement" name="k2_en">N/A</textarea>
                                         @error('k2_en')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -95,7 +95,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="k2_se">k2 @lang('messages.inSwedish') [@lang('messages.optional')]</label>
-                                        <textarea id="k2_se" class="form-control @error('k2_se') is-invalid @enderror" rows="3" placeholder="Vi har en strategi eller handlingsplan, alternativt reglerat det genom avtal" name="k2_se">{{ old('k2_se') }}</textarea>
+                                        <textarea id="k2_se" class="form-control @error('k2_se') is-invalid @enderror" rows="3" placeholder="Vi har en strategi eller handlingsplan, alternativt reglerat det genom avtal" name="k2_se">N/A</textarea>
                                         @error('k2_se')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -104,7 +104,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="k3_en">k3 @lang('messages.inEnglish') [@lang('messages.optional')]</label>
-                                        <textarea id="k3_en" class="form-control @error('k3_en') is-invalid @enderror" rows="3" placeholder="We have a strategy or action plan, or we have regulated it by agreement" name="k3_en">{{ old('k3_en') }}</textarea>
+                                        <textarea id="k3_en" class="form-control @error('k3_en') is-invalid @enderror" rows="3" placeholder="We have a strategy or action plan, or we have regulated it by agreement" name="k3_en">N/A</textarea>
                                         @error('k3_en')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -113,7 +113,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="k3_se">k3 @lang('messages.inSwedish') [@lang('messages.optional')]</label>
-                                        <textarea id="k3_se" class="form-control @error('k3_se') is-invalid @enderror" rows="3" placeholder="Vi har en strategi eller handlingsplan, alternativt reglerat det genom avtal" name="k3_se">{{ old('k3_se') }}</textarea>
+                                        <textarea id="k3_se" class="form-control @error('k3_se') is-invalid @enderror" rows="3" placeholder="Vi har en strategi eller handlingsplan, alternativt reglerat det genom avtal" name="k3_se">N/A</textarea>
                                         @error('k3_se')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -122,7 +122,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="k4_en">k4 @lang('messages.inEnglish') [@lang('messages.optional')]</label>
-                                        <textarea id="k4_en" class="form-control @error('k4_en') is-invalid @enderror" rows="3" placeholder="We have a strategy or action plan, or we have regulated it by agreement" name="k4_en">{{ old('k4_en') }}</textarea>
+                                        <textarea id="k4_en" class="form-control @error('k4_en') is-invalid @enderror" rows="3" placeholder="We have a strategy or action plan, or we have regulated it by agreement" name="k4_en">N/A</textarea>
                                         @error('k4_en')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -131,7 +131,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="k4_se">k4 @lang('messages.inSwedish') [@lang('messages.optional')]</label>
-                                        <textarea id="k4_se" class="form-control @error('k4_se') is-invalid @enderror" rows="3" placeholder="Vi har en strategi eller handlingsplan, alternativt reglerat det genom avtal" name="k4_se">{{ old('k4_se') }}</textarea>
+                                        <textarea id="k4_se" class="form-control @error('k4_se') is-invalid @enderror" rows="3" placeholder="Vi har en strategi eller handlingsplan, alternativt reglerat det genom avtal" name="k4_se">N/A</textarea>
                                         @error('k4_se')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -140,7 +140,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="k5_en">k5 @lang('messages.inEnglish') [@lang('messages.optional')]</label>
-                                        <textarea id="k5_en" class="form-control @error('k5_en') is-invalid @enderror" rows="3" placeholder="We have a strategy or action plan, or we have regulated it by agreement" name="k5_en">{{ old('k5_en') }}</textarea>
+                                        <textarea id="k5_en" class="form-control @error('k5_en') is-invalid @enderror" rows="3" placeholder="We have a strategy or action plan, or we have regulated it by agreement" name="k5_en">N/A</textarea>
                                         @error('k5_en')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -149,7 +149,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="k5_se">k5 @lang('messages.inSwedish') [@lang('messages.optional')]</label>
-                                        <textarea id="k5_se" class="form-control @error('k5_se') is-invalid @enderror" rows="3" placeholder="Vi har en strategi eller handlingsplan, alternativt reglerat det genom avtal" name="k5_se">{{ old('k5_se') }}</textarea>
+                                        <textarea id="k5_se" class="form-control @error('k5_se') is-invalid @enderror" rows="3" placeholder="Vi har en strategi eller handlingsplan, alternativt reglerat det genom avtal" name="k5_se">N/A</textarea>
                                         @error('k5_se')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -158,7 +158,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="implementation_en">@lang('messages.implementation') @lang('messages.inEnglish')</label>
-                                        <textarea id="implementation_en" class="form-control @error('implementation_en') is-invalid @enderror" rows="3" placeholder="Show the user (DPO) how to implement a plan for this statement" name="implementation_en">{{ old('implementation_en') }}</textarea>
+                                        <textarea id="implementation_en" class="form-control @error('implementation_en') is-invalid @enderror" rows="3" placeholder="Show the user (DPO) how to implement a plan for this statement" name="implementation_en">N/A</textarea>
                                         @error('implementation_en')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -167,7 +167,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="implementation_se">@lang('messages.implementation') @lang('messages.inSwedish')</label>
-                                        <textarea id="implementation_se" class="form-control @error('implementation_se') is-invalid @enderror" rows="3" placeholder="Visa användaren (DPO) hur man implementerar en plan för denna programsats" name="implementation_se">{{ old('implementation_se') }}</textarea>
+                                        <textarea id="implementation_se" class="form-control @error('implementation_se') is-invalid @enderror" rows="3" placeholder="Visa användaren (DPO) hur man implementerar en plan för denna programsats" name="implementation_se">N/A</textarea>
                                         @error('implementation_se')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -176,7 +176,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="guide_en">@lang('messages.guide_en')</label>
-                                        <textarea id="guide_en" class="form-control @error('guide_en') is-invalid @enderror" rows="3" placeholder="Guide the auditor (DSO) how to review this statement" name="guide_en">{{ old('guide_en') }}</textarea>
+                                        <textarea id="guide_en" class="form-control @error('guide_en') is-invalid @enderror" rows="3" placeholder="Guide the auditor (DSO) how to review this statement" name="guide_en">N/A</textarea>
                                         @error('guide_en')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
@@ -185,7 +185,7 @@
                                 <div class="col-md-6 col-12">
                                     <div class="mb-1">
                                         <label class="form-label" for="guide_se">@lang('messages.guide_se')</label>
-                                        <textarea id="guide_se" class="form-control @error('guide_se') is-invalid @enderror" rows="3" placeholder="Vägleda revisorn (DSO) hur man granskar detta uttalande" name="guide_se">{{ old('guide_se') }}</textarea>
+                                        <textarea id="guide_se" class="form-control @error('guide_se') is-invalid @enderror" rows="3" placeholder="Vägleda revisorn (DSO) hur man granskar detta uttalande" name="guide_se">N/A</textarea>
                                         @error('guide_se')
                                             <div class="invalid-feedback">{{ $message }}</div>
                                         @enderror
